b5f563e7f84d86f28de3ad3126aa8a16966d5728
AgusPedernera <agustinapedernera97@gmail.com>
2024-01-17 00:11:19 -0300

    Tratamos de conectar back con front

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const PORT = config.PORT;
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5173/', // Reemplaza con la URL de tu frontend
-    methods: 'GET',
+    origin: 'http://localhost:5173', // Reemplaza con la URL de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Permite el envío de cookies
     optionsSuccessStatus: 204, // Configura el código de estado para las solicitudes OPTIONS preflight
     allowedHeaders: 'Content-Type,Authorization', // Especifica los encabezados permitidos
